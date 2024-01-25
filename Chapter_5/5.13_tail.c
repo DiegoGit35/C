6e85cb5e4375f111d83211f7b9f39e429607d094
Diego Estrada <diegoestrada35@gmail.com>
2024-01-25 03:55:11 -0300

    5.13 done

--- Chapter_5/5.13_tail.c
+++ Chapter_5/5.13_tail.c
@@ -3,22 +3,22 @@ is set to 10, let us say, but it can be changed by an optional argument so that
     tail -n
 prints the last n lnes. The program should behave rationally no matter how unreasonable the
 input or the value n. Write the program so it makes the best use of available storage; lines
-should be stored as in the sorting program of Section 5.6, not in a two-dimentional array of 
+should be stored as in the sorting program of Section 5.6, not in a two-dimentional array of
 fixed size*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define DEFLINES 10 /* default # of lines to print */
+#define LINES 100   /* max # of lines to print */
+#define MAXLEN 100  /* max lenght of an input line */
 
-#define     DEFLINES    10  /* default # of lines to print */
-#define     LINES       100 /* max # of lines to print */
-#define     MAXLEN      100 /* max lenght of an input line */
-
+int getLine(char [], int );
 void error(char *);
 int getLine(char *, int);
 
-
-int main (int argc , char *argv[]){
+int main(int argc, char *argv[])
+{
     char *p;
     char *buf;
     char *bufend;
@@ -26,7 +26,62 @@ int main (int argc , char *argv[]){
     char *lineptr[LINES];
     int first, i, last, len, n, nlines;
 
+    printf("argc vale: %d\n", argc);
+
     if (argc == 1)
         printf("error, no hay suficientes argumentos\n");
-    else if(*(++argv)[0] == '-')
+    else if (*(++argv)[0] == '-' && argc == 3)
+    {
+        n = atoi(*++argv);
+        printf("*argv vale: %s\n", *argv);
+    }
+    else
+        n = DEFLINES;
+    printf("n vale: %d\n", n);
+
+    for (i = 0; i < LINES; i++)
+        lineptr[i] = NULL;
+    if ((p = buf = malloc(LINES * MAXLEN)) == NULL)
+        error("fail: cannot allocate buf");
+    bufend = buf + LINES * MAXLEN;
+    last = 0;
+    nlines = 0;
+    while ((len = getLine(line, MAXLEN)) > 0)
+    {
+        if (p + len + 1 >= bufend)
+            p = buf;
+        lineptr[last] = p;
+        strcpy(lineptr[last], line);
+        if (++last >= LINES)
+            last = 0;
+        p += len + 1;
+        nlines++;
+    }
+    if (n > nlines)
+        n = nlines;
+    first = last - n;
+    if (first < 0)
+        first += LINES;
+    for (i = first; n-- > 0; i = (i + 1) % LINES)
+        printf("%s", lineptr[i]);
+    return 0;
+}
+
+int getLine(char s[], int lim){
+    int c, i;
+
+    for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n'){
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void error(char *s)
+{
+    printf("%s\n", s);
+    exit(1);
 }
