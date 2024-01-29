ab7a64332ab5007f5c4b611e1cba1b0ef4dfb836
diego <diegoestrada35@gmail.com>
2024-01-29 08:55:56 -0300

    5.15 done

--- Chapter_5/5.15_upper_lower_case.c
+++ Chapter_5/5.15_upper_lower_case.c
@@ -1,10 +1,11 @@
-/* Modify the sort program to handle a -r flag, which indicates sorting in reverse
-(decreasing) order. Be sure that -r works with -n */
+/* Add the option -f to fold upper and lower case together, so that case
+distinctions are not made during sorting; for example a and A compare equal */
 
 #include <stdio.h>
 #include <string.h>
 
 #define MAXLINES 5000
+#define MAXLEN 200
 char *lineptr[MAXLINES];
 
 int getLine(char s[], int lim);
@@ -12,6 +13,7 @@ int readLines(char *lineptr[], int nlines);
 void writeLines(char *lineptr[], int nlines);
 void Qsort(void *lineptr[], int left, int right, int (*comp)(void *, void *));
 int numcmp(const char *, const char *);
+int Mystrcmp(const char *, const char *);
 void Rev(void *lineptr[], int nlines);
 
 int main(int argc, char const *argv[])
@@ -19,6 +21,7 @@ int main(int argc, char const *argv[])
     int nlines;
     int numeric = 0;
     int reverse = 0;
+    int fold = 0;
 
     for (int i = 0; i < argc; i++)
     {
@@ -32,11 +35,17 @@ int main(int argc, char const *argv[])
             reverse = 1;
             continue;
         }
+        if (strcmp(argv[i], "-f") == 0)
+        {
+            printf("-f mentioned\n");
+            fold = 1;
+            continue;
+        }
     }
 
     if ((nlines = readLines(lineptr, MAXLINES)) >= 0)
     {
-        Qsort((void **)lineptr, 0, nlines - 1, (int (*)(void *, void *))(numeric ? numcmp : strcmp));
+        Qsort((void **)lineptr, 0, nlines - 1, (int (*)(void *, void *))(numeric ? numcmp : (fold ? Mystrcmp : strcmp)));
         if (reverse == 1)
             Rev((void **)lineptr, nlines);
         writeLines(lineptr, nlines);
@@ -64,7 +73,27 @@ int numcmp(const char *s1, const char *s2)
         return 0;
 }
 
-#define MAXLEN 1000
+int Mystrcmp(const char *s1, const char *s2)
+{
+    int i = 0;
+    char a, b;
+    for (int i = 0; i < MAXLEN && s1[i] != '\0' && s2[i] != '\0'; i++)
+    {
+        a = s1[i];
+        b = s2[i];
+        if (64 < s1[i] && s1[i] < 91)
+            a += 32;
+        if (64 < s2[i] && s2[i] < 91)
+            b += 32;
+        if (a - b == 0)
+            continue;
+        else
+        {
+            return a - b;
+        }
+    }
+}
+
 void swap(void *v[], int, int);
 
 int readLines(char *lineptr[], int maxlines)
